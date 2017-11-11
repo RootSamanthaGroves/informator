d89a7a3157343e1d55d62e0d2cd7a53be97609ea
RootSamanthaGroves <dominika.sporzynska@gmail.com>
2017-11-11 18:14:25 +0100

    pierwsyz

---
+++ src/main/java/com/example/informator/InformatorApplication.java
@@ -0,0 +1,12 @@
+package com.example.informator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformatorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/informator/InformatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.informator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InformatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
